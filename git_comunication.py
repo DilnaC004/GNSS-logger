58a88b8fd44be97a6d547494d31a3013c52d4dba
= <=>
2021-01-15 16:06:24 +0100

    update git synchronization

--- git_comunication.py
+++ git_comunication.py
@@ -1,32 +1,50 @@
 import os
 import git
+import urllib.request
 
 
 def synchronize_data():
+    '''
+    Function check if actual directory is Git repository and
+    if is, synchronize all untracked files to remote repository.
+    '''
     if check_git_directory():
-        # inicitalizace repositare
+        # Initialize repository
         r = git.Repo.init(os.path.curdir)
-        # vyhledani netrackovanych souboru
+        # Find untracked files and idexing them
         untracked_files = r.untracked_files
-        # vytvoreni commitu
-        for uF in untracked_files:
-            r.index.add(uF)
-        # nahrani na server - nutne otestovat co se stane, kdyz neni internet
+        if len(untracked_files) > 0:
+            for uF in untracked_files:
+                r.index.add(uF)
+            # Create commit
+            r.index.commit("Upload new data logs")
+            # Upload data to remote Repository - nutne otestovat co se stane, kdyz neni internet
+            if internet_connection():
+                r.remotes.origin.push()
+                print("Git - New files uploaded :", untracked_files)
+            else:
+                print("Git - Cannot synchronize data - no internet connection")
 
-     #print("neco nefunguje")
+        else:
+            print("Git - No changes in repository")
+
+
+def internet_connection(host='http://google.com'):
+    try:
+        urllib.request.urlopen(host)
+        return True
+    except:
+        return False
 
 
 def check_git_directory():
 
     if os.path.isdir(".git"):
-        print("Current folder is Git repository")
         return True
     else:
-        print("Current folder isn't Git repository")
+        print("Git - Current folder isn't Git repository")
         return False
 
 
 if __name__ == "__main__":
-    # print(os.path.curdir)
-    # check_git_directory()
     synchronize_data()
--- main.py
+++ main.py
@@ -2,61 +2,84 @@ import time
 import serial
 import threading
 import pynmea2
+import os
+import re
+from git_comunication import synchronize_data
 
 
 class SerialNmeaRead(threading.Thread):
     '''
     The class with the method that reads the serial port in the backgroud.
     '''
-    def __init__(self,com_port,baudrate=38400):
+
+    def __init__(self, com_port, baudrate=38400):
         super().__init__()
         self._stop_event = threading.Event()
         self.serial_object = serial.Serial(com_port, baudrate)
         self.file_name = ""
 
-    def define_file_name(nmea_timestamp):
-        str_date = time.strftime("%Y_%m_%d",time.gmtime())
-        ## predelat na hodiny
-        str_time = str(nmea_timestamp)[0:-3].replace(":","_") # delete seconds
-        new_file_name = str_date + "_" + str_time + ".log"
+    def define_file_name(self, gga_time):
+
+        logging_path = "LOGS"
+        if not os.path.exists(logging_path):
+            try:
+                os.mkdir(logging_path)
+            except OSError:
+                print("Creation of the directory %s failed" % logging_path)
+            else:
+                print("Successfully created the directory %s " % logging_path)
+
+        str_date = time.strftime("%Y_%m_%d", time.localtime()+3600)
+        # predelat na hodiny# delete seconds
+        str_time = str(gga_time)[0:-3].replace(":", "_")
+        new_file_name = logging_path + os.path.sep + str_date + "_" + str_time + ".log"
 
         if self.file_name == "":
             self.file_name = new_file_name
-        elif self.file_name == new_file_name:
+        elif self.file_name != new_file_name:
             self.file_name = new_file_name
-            print("odeslani dat na git")
+            # synchronize_data()
+            print("odeslani starych dat na git")
 
+    def get_GGA_timestamp(self, serial_data):
 
-    
+        match = re.search("\$GNGGA.*\*..", serial_data)
+
+        if match:
+            GGA_message = serial_data[match.start():match.end()]
+            GGA_parse = pynmea2.parse(GGA_message)
+            self.define_file_name(GGA_parse.timestamp)
+
     def run(self):
         '''
         The method that actually gets data from the port
         '''
         while not self.stopped():
-            datos = self.serial_object.readline().decode('ascii')
-            nmea = pynmea2.parse(datos)
+            serial_data = self.serial_object.readline().decode('ascii')
 
-            if nmea.sentence_type == "GGA":
-                self.define_file_name(str(nmea.timestamp))
+            try:
 
-            if self.file_name is not "":
-                with open(self.file_name, "a", encoding="utf-8") as f:
-                    f.write(datos.strip("\n"))
-    
+                self.get_GGA_timestamp(serial_data)
+
+                if self.file_name != "":
+                    with open(self.file_name, "a", encoding="utf-8") as f:
+                        f.write(serial_data.strip("\n"))
+
+            except:
+                print('Some error in data: ', serial_data)
+
     def stop(self):
         self._stop_event.set()
         self.serial_object.close()
-        
+
     def stopped(self):
         return self._stop_event.is_set()
 
 
-
-
-serial_thread = SerialNmeaRead('COM7',38400)
+serial_thread = SerialNmeaRead('COM5', 38400)
 serial_thread.start()
 
-x = input('Enter anything to cancel window:')
+x = input('Enter anything to cancel window:\n')
 
 serial_thread.stop()
 print("Thread stopped.")
